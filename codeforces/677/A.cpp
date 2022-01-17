d672c08830d5ab335150317a099fb90924abc977
DEEP NARAYAN <narayan9deep@gmail.com>
2022-01-17 13:05:00 +0530

    Add solution for problem `A - Vanya and Fence` Link: https://codeforces.com/contest/677/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/677/submission/143080997

--- codeforces/677/A.cpp
+++ codeforces/677/A.cpp
@@ -1,52 +1,36 @@
-#include <bits/stdc++.h>
-
-#define fastio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define nline "\n"
-
+#include<bits/stdc++.h>
 using namespace std;
 
-void solve()
-{
-    int n,h;
-    cin>>n>>h;
-    int ans = 0;
-    int arr[n];
-    for (int i = 0;i<n;i++)
-    {
-        cin>>arr[i];
-        if(arr[i]<=h) ans+=1;
-        else ans+=2;
-    }
-    cout<<ans;
-}
+
 
 int main()
 {
-	fastio();
-	#ifndef ONLINE_JUDGE
-		freopen("input.txt",  "r",  stdin);
-		freopen("output.txt", "w", stdout);
-        freopen("Error.txt", "w", stderr);
-	#endif
-	clock_t z = clock();
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-	//write code here:
+    int n, h;
+    cin>>n>>h;
+    int arr[n];
+    for (int i = 0; i < n; ++i)
+    {
+        /* code */
+        cin>>arr[i];
+    }
+    int width = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        /* code */
+        if(arr[i]>h)
+        {
+            width=width+2;
+        }
+        else
+        {
+            width = width+1;
+        }
+    }
+    cout<<width;
+ 
 
-        // //if number of testcases present:
-        //     //input number of test cases:
-        //     int tc;
-        //     cin>>tc;
-        //     while(tc--)
-        //     {
-        //         solve();
-        //     }
-
-        //if number of testcases not present
-        solve();
-
-    #ifndef ONLINE_JUDGE
-		cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
-	#endif
-	
-	return 0;
+    return 0;
 }
